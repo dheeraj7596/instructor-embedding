420664c350a2e8b4b1e36737400dfcb7f75369d6
dheerajmekala <dheeraj7596.dm@gmail.com>
2023-05-24 23:54:12 -0700

    pythonpath added.

--- evaluation/MTEB/examples/scripts/run_all.sh
+++ evaluation/MTEB/examples/scripts/run_all.sh
@@ -1,3 +1,4 @@
+export PYTHONPATH="${PYTHONPATH}:/home/dheeraj/instructor-embedding/evaluation/MTEB/mteb"
 sh examples/scripts/classification.sh
 sh examples/scripts/clustering.sh
 sh examples/scripts/pairwiseclassification.sh
